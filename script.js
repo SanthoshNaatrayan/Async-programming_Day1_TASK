4f8dde4afb5e9f75df6fd5be8ad8244862bae0cf
SanthoshNaatryan <santhoshnaatrayan@gmail.com>
2024-01-24 10:18:21 +0530

    added files to git

---
+++ script.js
@@ -0,0 +1,128 @@
+let a = document.createElement('h1');
+a.innerText = '10';
+a.style.cssText = 'color:RED; text-align:center; font-size:250px';
+
+let b = document.createElement('h1');
+b.innerText = '9';
+b.style.cssText = 'color:BLUE; text-align:center; font-size:250px';
+
+let c = document.createElement('h1');
+c.innerText = '8';
+c.style.cssText = 'color:BROWN; text-align:center; font-size:250px';
+
+let d = document.createElement('h1');
+d.innerText = '7';
+d.style.cssText = 'color:PINK; text-align:center; font-size:250px';
+
+let e = document.createElement('h1');
+e.innerText = '6';
+e.style.cssText = 'color:VIOLET; text-align:center; font-size:250px';
+
+let e1 = document.createElement('h1');
+e1.style.cssText = 'display:none';
+
+let f = document.createElement('h1');
+f.innerText = '5';
+f.style.cssText = 'color:GREY; text-align:center; font-size:250px';
+
+let f1 = document.createElement('h1');
+f1.style.cssText = 'display:none';
+
+let g = document.createElement('h1');
+g.innerText = '4';
+g.style.cssText = 'color:BLACK; text-align:center; font-size:250px';
+
+let g1 = document.createElement('h1');
+g1.style.cssText = 'display:none';
+
+let h = document.createElement('h1');
+h.innerText = '3';
+h.style.cssText = 'color:SKYBLUE; text-align:center; font-size:250px';
+
+let h1 = document.createElement('h1');
+h1.style.cssText = 'display:none';
+
+let i = document.createElement('h1');
+i.innerText = '2';
+i.style.cssText = 'color:RED; text-align:center; font-size:250px';
+
+let i1 = document.createElement('h1');
+i1.style.cssText = 'display:none';
+
+let j = document.createElement('h1');
+j.innerText = '1';
+j.style.cssText = 'color:YELLOW; text-align:center; font-size:250px';
+
+let j1 = document.createElement('h1');
+j1.style.cssText = 'display:none';
+
+let k = document.createElement('h1');
+k.innerText = 'Happy Independence Day';
+k.style.cssText = 'background-color:green; color:rgb(231, 85, 12); text-align:center; font-size:150px ';
+
+
+
+setTimeout(() => {
+  document.body.append(a)
+  setTimeout(() => {
+    a.style.cssText = 'display:none'
+    setTimeout(() => {
+      document.body.append(b)
+      setTimeout(() => {
+        b.style.cssText = 'display:none'
+        setTimeout(() => {
+          document.body.append(c)
+          setTimeout(() => {
+            c.style.cssText = 'display:none'
+            setTimeout(() => {
+              document.body.append(d)
+              setTimeout(() => {
+                d.style.cssText = 'display:none'
+                setTimeout(() => {
+                  document.body.append(e)
+                  setTimeout(() => {
+                    e.style.cssText = 'display:none'
+                    setTimeout(() => {
+                      document.body.append(f)
+                      setTimeout(() => {
+                        f.style.cssText = 'display:none'
+                        setTimeout(() => {
+                          document.body.append(g)
+                          setTimeout(() => {
+                            g.style.cssText = 'display:none'
+                            setTimeout(() => {
+                              document.body.append(h)
+                              setTimeout(() => {
+                                h.style.cssText = 'display:none'
+                                setTimeout(() => {
+                                  document.body.append(i)
+                                  setTimeout(() => {
+                                    i.style.cssText = 'display:none'
+                                    setTimeout(() => {
+                                      document.body.append(j)
+                                      setTimeout(() => {
+                                        j.style.cssText = 'display:none'
+                                        setTimeout(() => {
+                                          document.body.append(k)
+
+                                        }, 1000)
+                                      }, 1000)
+                                    }, 1000)
+                                  }, 1000)
+                                }, 1000)
+                              }, 1000)
+                            }, 1000)
+                          }, 1000)
+                        }, 1000)
+                      }, 1000)
+                    }, 1000)
+                  }, 1000)
+                }, 1000)
+              }, 1000)
+            }, 1000)
+          }, 1000)
+        }, 1000)
+      }, 1000)
+    }, 1000)
+  }, 1000)
+}, 1000)
